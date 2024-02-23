909f7869ac5e78d66496f91ec9397da421484055
İbrahim Serhat Bulut <96337247+actanonvebra@users.noreply.github.com>
2024-02-23 04:41:50 +0300

    Add files via upload

---
+++ gopixdeneme/config/config.go
@@ -0,0 +1,74 @@
+package config
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+//bazı fonksiyonlar bazı fonksiyonların içinde kullanılacak.
+
+// verilen path doğru mu?
+func pathExists(filePath string) bool {
+	_, err := os.Stat(filePath)
+	if !os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+// dosyam var mı?
+func fileExists(fileName string) (bool, string) {
+	_, err := os.Stat(fileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, ""
+		}
+	}
+	return true, fileName
+
+}
+
+type Config struct {
+	ClientID     string `yaml:"clientID"`
+	ClientSecret string `yaml:"clientSecret"`
+}
+
+// dosyayı oluşturmak
+func CreateConfigFile(clientID string, clientSecret string) {
+
+	configg := Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+	}
+
+	yamlFile, err := yaml.Marshal(&configg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("config.yaml", yamlFile, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+// bu dosya var olan config.yaml içini okuyup config yapısına atacak.
+func ReadConfigFile() (Config, error) {
+	var c Config
+
+	yamlFile, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		return c, err
+	}
+
+	err = yaml.Unmarshal(yamlFile, &c)
+	if err != nil {
+		return c, err
+	}
+	return c, nil
+
+}
